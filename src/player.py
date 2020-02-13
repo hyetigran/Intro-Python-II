0583b0f331e0d6a1174e8773820c4e2760e6ccec
hyetigran <tigran.asriyan@gmail.com>
2020-02-13 15:41:25 -0500

    add get and drop methods to the player class

--- src/player.py
+++ src/player.py
@@ -3,9 +3,10 @@
 
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, item):
         self.name = name
         self.current_room = current_room
+        self.items = [item]
 
     def travel(self, direction):
         next_room = getattr(current_room, f"{direction}_to")
@@ -13,3 +14,11 @@ class Player:
             player.current_room = next_room
         else:
             print("You cannot move in that directoin.")
+
+    # * Add `get [ITEM_NAME]` and `drop [ITEM_NAME]` commands to the parser
+    def get_item(self, item):
+        return player.item.append(item)
+
+    def drop_item(self, item):
+        items = player.items
+        return [i for i in items if i != item]
